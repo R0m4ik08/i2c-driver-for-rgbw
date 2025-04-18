dce26bdb932febd3f595886c9b70b62cafb0f3a4
Бугаев Роман <roman15082002@mail.ru>
2025-04-18 23:57:42 +0300

    feat: добавляет прототипы функций rgbw драйвера

---
+++ Core/Inc/rgbw_driver.h
@@ -0,0 +1,28 @@
+#ifndef INC_RGBW_DRIVER_H_
+#define INC_RGBW_DRIVER_H_
+
+#include <stdint.h>
+
+#include "stm32f3xx_hal.h"
+
+typedef enum{
+	RED		= 0b1,
+	GREEN	= 0b10,
+	BLUE	= 0b100,
+	WHITE	= 0b1000
+}rgbw_driver_channels_t;
+
+extern uint8_t rgb_chip_i2c_adress;
+
+HAL_I2C_StateTypeDef rgbw_driver_init(I2C_HandleTypeDef* hi2c_dev);
+
+HAL_I2C_StateTypeDef rgbw_driver_set_chanal_brightness(rgbw_driver_channels_t channel, uint8_t brightness);
+HAL_I2C_StateTypeDef rgbw_driver_set_rgb_color(uint8_t red, uint8_t green, uint8_t blue);
+
+HAL_I2C_StateTypeDef rgbw_driver_channel_enable(rgbw_driver_channels_t channel);
+HAL_I2C_StateTypeDef rgbw_driver_channel_disable(rgbw_driver_channels_t channel);
+
+HAL_I2C_StateTypeDef rgbw_driver_all_channel_enable();
+HAL_I2C_StateTypeDef rgbw_driver_all_channel_disable();
+
+#endif
