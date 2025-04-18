20635d4858125730157c52f292fed965b4a7e7e7
Бугаев Роман <roman15082002@mail.ru>
2025-04-19 01:22:29 +0300

    feat: добавляет функцию для установки яркости канала RGBW

--- Core/Src/rgbw_driver.c
+++ Core/Src/rgbw_driver.c
@@ -39,3 +39,46 @@ HAL_StatusTypeDef rgbw_driver_init(I2C_HandleTypeDef* hi2c_dev){
 	return transmitting_result;
 }
 
+HAL_StatusTypeDef rgbw_driver_set_channel_brightness(rgbw_driver_channels_t channel, uint8_t brightness){
+	
+	// Проверка на то, что была ли инициализирована микросхема управления RGBW
+	if(rgbw_driver_hi2c_dev == NULL){
+		return HAL_ERROR;
+	}
+
+	HAL_StatusTypeDef result =  HAL_OK;
+	rgbw_chip_registers_t tmp_reg_addres;
+	uint8_t tmp_data[2];
+
+	// Определяем адрес регистра, в который будем записывать яркость канала
+	switch(channel){
+		case RED:
+			tmp_reg_addres = RGBW_CHIP_REG_CHANNEL_RED;
+			break;
+		case GREEN:
+			tmp_reg_addres = RGBW_CHIP_REG_CHANNEL_GREEN;
+			break;
+		case BLUE:
+			tmp_reg_addres = RGBW_CHIP_REG_CHANNEL_BLUE;
+			break;
+		case WHITE:
+			tmp_reg_addres = RGBW_CHIP_REG_CHANNEL_WHITE;
+			break;
+		default:
+			return HAL_ERROR;
+	}
+
+	// Формируем данные(2 байта) для передачи по I2C
+
+	tmp_data[0] = (uint8_t) tmp_reg_addres;
+	tmp_data[1] = brightness;
+
+	// Отправляем данные по I2C
+
+	result = HAL_I2C_Master_Transmit(rgbw_driver_hi2c_dev, (rgb_chip_i2c_adress << 1), tmp_data, 2, RGBW_DRIVER_I2C_TIMEOUT_MS);
+	if(result != HAL_OK){
+		return result;
+	}
+	
+	return result;
+}
