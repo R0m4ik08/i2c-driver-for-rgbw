6ed42e6513c4e511353b76c2016f52022dab65fa
Бугаев Роман <roman15082002@mail.ru>
2025-04-19 23:19:02 +0300

    refactor: выделяет функцию для получения адреса регистра канала и упрощает логику установки яркости канала

--- Core/Src/rgbw_driver.c
+++ Core/Src/rgbw_driver.c
@@ -4,6 +4,21 @@ uint8_t rgb_chip_i2c_adress = 0x0A;
 
 I2C_HandleTypeDef* rgbw_driver_hi2c_dev = NULL;
 
+rgbw_chip_registers_t get_addres_from_channel(rgbw_driver_channels_t channel){
+
+    switch(channel){
+        case RED:
+            return RGBW_CHIP_REG_CHANNEL_RED;
+        case GREEN:
+            return RGBW_CHIP_REG_CHANNEL_GREEN;
+        case BLUE:
+            return RGBW_CHIP_REG_CHANNEL_BLUE;
+        case WHITE:
+            return RGBW_CHIP_REG_CHANNEL_WHITE;
+        default:
+            return RGBW_CHIP_REG_INCORRECT;
+    }
+}
 
 HAL_StatusTypeDef rgbw_driver_init(I2C_HandleTypeDef* hi2c_dev){
 
@@ -50,23 +65,11 @@ HAL_StatusTypeDef rgbw_driver_set_channel_brightness(rgbw_driver_channels_t chan
 	rgbw_chip_registers_t tmp_reg_addres;
 	uint8_t tmp_data[2];
 
-	// Определяем адрес регистра, в который будем записывать яркость канала
-	switch(channel){
-		case RED:
-			tmp_reg_addres = RGBW_CHIP_REG_CHANNEL_RED;
-			break;
-		case GREEN:
-			tmp_reg_addres = RGBW_CHIP_REG_CHANNEL_GREEN;
-			break;
-		case BLUE:
-			tmp_reg_addres = RGBW_CHIP_REG_CHANNEL_BLUE;
-			break;
-		case WHITE:
-			tmp_reg_addres = RGBW_CHIP_REG_CHANNEL_WHITE;
-			break;
-		default:
-			return HAL_ERROR;
-	}
+    // Определяем адрес регистра, в который будем записывать яркость канала
+    tmp_reg_addres = get_addres_from_channel(channel);
+    if(tmp_reg_addres == RGBW_CHIP_REG_INCORRECT){
+        return HAL_ERROR;
+    }
 
 	// Формируем данные(2 байта) для передачи по I2C
 
