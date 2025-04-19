aa02905b38bd48d6efbe483c74343bd5442ba24e
Бугаев Роман <roman15082002@mail.ru>
2025-04-19 23:11:59 +0300

    refactor: заменяет прототипы функции включения и отключения канала на универсальную функцию активности канала

--- Core/Inc/rgbw_driver.h
+++ Core/Inc/rgbw_driver.h
@@ -33,8 +33,7 @@ HAL_StatusTypeDef rgbw_driver_init(I2C_HandleTypeDef* hi2c_dev);
 HAL_StatusTypeDef rgbw_driver_set_channel_brightness(rgbw_driver_channels_t channel, uint8_t brightness);
 HAL_StatusTypeDef rgbw_driver_set_rgb_color(uint8_t red, uint8_t green, uint8_t blue);
 
-HAL_StatusTypeDef rgbw_driver_channel_enable(rgbw_driver_channels_t channel);
-HAL_StatusTypeDef rgbw_driver_channel_disable(rgbw_driver_channels_t channel);
+HAL_StatusTypeDef rgbw_driver_set_channel_activity(rgbw_driver_channels_t channel, bool activity);
 
 HAL_StatusTypeDef rgbw_driver_all_channel_enable();
 HAL_StatusTypeDef rgbw_driver_all_channel_disable();
