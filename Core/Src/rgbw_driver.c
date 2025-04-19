1b6c2b2afcd529c806d0f8632b992c3a1ebeea67
Бугаев Роман <roman15082002@mail.ru>
2025-04-19 23:23:30 +0300

    style: заменяет отступы на пробелы в файле rgbw_driver.c

--- Core/Src/rgbw_driver.c
+++ Core/Src/rgbw_driver.c
@@ -22,48 +22,47 @@ rgbw_chip_registers_t get_addres_from_channel(rgbw_driver_channels_t channel){
 
 HAL_StatusTypeDef rgbw_driver_init(I2C_HandleTypeDef* hi2c_dev){
 
-	rgbw_chip_registers_t tmp_reg_addres;
-	uint8_t tmp_data[2];
+    rgbw_chip_registers_t tmp_reg_addres;
+    uint8_t tmp_data[2];
 
-	// Сохраняем указатель на I2C
+    // Сохраняем указатель на I2C
 
-	rgbw_driver_hi2c_dev = hi2c_dev;
+    rgbw_driver_hi2c_dev = hi2c_dev;
 
-	// Включаем микросхему
+    // Включаем микросхему
 
-	tmp_reg_addres = RGBW_CHIP_REG_ENABLED;
-	tmp_data[0] = (uint8_t) tmp_reg_addres;
-	tmp_data[1] = 0x01;
+    tmp_reg_addres = RGBW_CHIP_REG_ENABLED;
+    tmp_data[0] = (uint8_t) tmp_reg_addres;
+    tmp_data[1] = 0x01;
 
-	HAL_StatusTypeDef transmitting_result = HAL_I2C_Master_Transmit(hi2c_dev, (rgb_chip_i2c_adress << 1), tmp_data, 2, RGBW_DRIVER_I2C_TIMEOUT_MS);
-	if(transmitting_result != HAL_OK){
-		return transmitting_result;
-	}
+    HAL_StatusTypeDef transmitting_result = HAL_I2C_Master_Transmit(hi2c_dev, (rgb_chip_i2c_adress << 1), tmp_data, 2, RGBW_DRIVER_I2C_TIMEOUT_MS);
+    if(transmitting_result != HAL_OK){
+        return transmitting_result;
+    }
 
-	// Гасим все каналы
+    // Гасим все каналы
 
-	tmp_reg_addres = RGBW_CHIP_REG_CNANNELS_MASK;
-	tmp_data[0] = (uint8_t) tmp_reg_addres;
-	tmp_data[1] = 0x00;
+    tmp_reg_addres = RGBW_CHIP_REG_CHANNELS_MASK;
+    tmp_data[0] = (uint8_t) tmp_reg_addres;
+    tmp_data[1] = 0x00;
 
-	transmitting_result = HAL_I2C_Master_Transmit(hi2c_dev, (rgb_chip_i2c_adress << 1), tmp_data, 2, RGBW_DRIVER_I2C_TIMEOUT_MS);
-	if(transmitting_result != HAL_OK){
-		return transmitting_result;
-	}
+    transmitting_result = HAL_I2C_Master_Transmit(hi2c_dev, (rgb_chip_i2c_adress << 1), tmp_data, 2, RGBW_DRIVER_I2C_TIMEOUT_MS);
+    if(transmitting_result != HAL_OK){
+        return transmitting_result;
+    }
 
-	return transmitting_result;
+    return transmitting_result;
 }
 
 HAL_StatusTypeDef rgbw_driver_set_channel_brightness(rgbw_driver_channels_t channel, uint8_t brightness){
-	
-	// Проверка на то, что была ли инициализирована микросхема управления RGBW
-	if(rgbw_driver_hi2c_dev == NULL){
-		return HAL_ERROR;
-	}
+    // Проверка на то, что была ли инициализирована микросхема управления RGBW
+    if(rgbw_driver_hi2c_dev == NULL){
+        return HAL_ERROR;
+    }
 
-	HAL_StatusTypeDef result =  HAL_OK;
-	rgbw_chip_registers_t tmp_reg_addres;
-	uint8_t tmp_data[2];
+    HAL_StatusTypeDef result =  HAL_ERROR;
+    rgbw_chip_registers_t tmp_reg_addres;
+    uint8_t tmp_data[2];
 
     // Определяем адрес регистра, в который будем записывать яркость канала
     tmp_reg_addres = get_addres_from_channel(channel);
@@ -71,17 +70,17 @@ HAL_StatusTypeDef rgbw_driver_set_channel_brightness(rgbw_driver_channels_t chan
         return HAL_ERROR;
     }
 
-	// Формируем данные(2 байта) для передачи по I2C
+    // Формируем данные(2 байта) для передачи по I2C
 
-	tmp_data[0] = (uint8_t) tmp_reg_addres;
-	tmp_data[1] = brightness;
+    tmp_data[0] = (uint8_t) tmp_reg_addres;
+    tmp_data[1] = brightness;
 
-	// Отправляем данные по I2C
+    // Отправляем данные по I2C
 
-	result = HAL_I2C_Master_Transmit(rgbw_driver_hi2c_dev, (rgb_chip_i2c_adress << 1), tmp_data, 2, RGBW_DRIVER_I2C_TIMEOUT_MS);
-	if(result != HAL_OK){
-		return result;
-	}
-	
-	return result;
+    result = HAL_I2C_Master_Transmit(rgbw_driver_hi2c_dev, (rgb_chip_i2c_adress << 1), tmp_data, 2, RGBW_DRIVER_I2C_TIMEOUT_MS);
+    if(result != HAL_OK){
+        return result;
+    }
+    
+    return result;
 }
