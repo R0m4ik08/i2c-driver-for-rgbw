99a313c2502ee3f8339f221b63cb0c67dd2e9b27
Бугаев Роман <roman15082002@mail.ru>
2025-04-20 15:58:36 +0300

    feat: добавляет реализацию функции HAL_I2C_Master_Transmit()
    Данная функция эмитирует работу микросхемы управления rgbw светодиодами на получение данных по i2c шине от микроконтроллера

--- pc_debug/Src/i2c.c
+++ pc_debug/Src/i2c.c
@@ -17,7 +17,31 @@ HAL_StatusTypeDef check_device_address(uint8_t DevAddress){
 }
 
 HAL_StatusTypeDef HAL_I2C_Master_Transmit(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout){
-    return HAL_OK;
+    
+    HAL_StatusTypeDef result = check_device_address(DevAddress);
+    if (result != HAL_OK) {
+        return result; // Возвращаем ошибку, если адрес устройства не совпадает
+    }
+
+    if (Size == 1){
+        rgbw_chip.address_reg_to_transmit = pData[0];
+        LOG("Address to transmit has been updated: %02X", rgbw_chip.address_reg_to_transmit);
+        return HAL_OK; // Возвращаем успех, если передан только адрес
+    }
+
+    if (Size == 2){
+        for (int i = 0; i < RGBW_CHIP_REGISTERS_COUNT; i++){
+            if (pData[0] == rgbw_chip.reg_addresses[i]){
+                rgbw_chip.reg_values[i] = pData[1];
+                LOG("Register %02X updated to %02X", pData[0], pData[1]);
+                return HAL_OK;
+            }
+        }
+        return HAL_OK;
+    }
+
+    LOG("Error: Invalid size for I2C transmission. Size: %d", Size);
+    return HAL_ERROR;
 }
 HAL_StatusTypeDef HAL_I2C_Master_Receive(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout){
     return HAL_OK;
