0ea91a5d99d4526acdc39f408efaf28452b564b4
Бугаев Роман <roman15082002@mail.ru>
2025-05-02 22:00:15 +0300

    feat: изменяет функцию инициализации драйвера в соответствии с новым описанием rgbw-микросхемы

--- Core/Src/rgbw_driver.c
+++ Core/Src/rgbw_driver.c
@@ -104,32 +104,57 @@ i2chw_error_t read_data_from_register_by_address(const i2chw_dev_t* i2chw_dev, r
 #pragma endregion
 
 #pragma region Public functions
+
+i2chw_error_t rgbw_driver_init(const i2chw_dev_t* i2chw_dev, const i2chw_cfg_t* i2chw_cfg )
+{
+    // Проверяет на ненулевые указатели
+
+    if ( i2chw_dev == NULL || i2chw_cfg == NULL){
+        return I2CHW_ERR_INVALID_PARAMS;
     }
+    
+    i2chw_error_t result;
+    
+    //  Конфигурирует i2c шину
 
-    HAL_StatusTypeDef result =  HAL_ERROR;
-    rgbw_chip_registers_t tmp_reg_addres = RGBW_CHIP_REG_CHANNELS_MASK;
-    uint8_t tmp_data[2];
+    result = I2CHW_Configure(i2chw_dev->bus_num, i2chw_cfg);
+    if (result != I2CHW_SUCCESS)
+    {
+        return result;
+    }
+    
+    //  Инициализирует i2c шину
 
-    // Запрашиваем текущее состояние каналов
-    result = get_current_channels_state(&tmp_data[1]);
-    if(result != HAL_OK){
+    result = I2CHW_Init(i2chw_dev->bus_num);
+    if (result != I2CHW_SUCCESS)
+    {
         return result;
     }
 
-    // Формируем данные(2 байта) для передачи по I2C
+    //  Отправляет данные в регистр сброса/конфигурации для полного сброса состояния rgb-микросхемы (0b111)
 
-    tmp_data[0]  = (uint8_t) tmp_reg_addres;
+    result = write_data_to_register_by_address(i2chw_dev, RGBW_CHIP_REG_RESET_CONTROL, (uint8_t) RESET_COMPLATE);
 
-    if (activity){
-        tmp_data[1] |= (uint8_t) channel;	
-    } else {
-        tmp_data[1] &= (uint8_t) (~channel);
-    }	
+    //  //  Игнорирует ошибку отсутствия бита подтверждения
 
-    // Отправляем данные по I2C
+    if (result != I2CHW_ERR_NACK && result != I2CHW_SUCCESS)
+    {
+        return result;
+    }
 
-    result = HAL_I2C_Master_Transmit(rgbw_driver_hi2c_dev, (rgb_chip_i2c_adress << 1), tmp_data, 2, RGBW_DRIVER_I2C_TIMEOUT_MS);
-    if(result != HAL_OK){
+    //  Ожидает окончание процесса сброса состояния (200 мкс по описанию микросхемы) !!! По факту ожидает 1мс
+    HAL_Delay(RGBW_DRIVER_I2C_TIME_FOR_RESET_MS); 
+
+    //  Отправляет данные в регистр сброса/конфигурации для установки режима: всегда включен (0b11000)
+    result = write_data_to_register_by_address(i2chw_dev, RGBW_CHIP_REG_RESET_CONTROL, (uint8_t) MODE_ALWAYS_ON);
+    if (result != I2CHW_SUCCESS)
+    {
+        return result;
+    }
+
+    return result;
+}
+
         return result;
     }
 
