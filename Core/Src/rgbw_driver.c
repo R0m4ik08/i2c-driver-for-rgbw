28bb255f0adb6015f524831393c4512eda424206
Бугаев Роман <roman15082002@mail.ru>
2025-05-02 21:42:24 +0300

    feat: добавляет функции реализующие процесс чтения и записи в регистр устройства используя интерфейс i2c протокола

--- Core/Src/rgbw_driver.c
+++ Core/Src/rgbw_driver.c
@@ -52,79 +52,81 @@ uint8_t get_white_part_from_rgb(uint8_t red, uint8_t green, uint8_t blue){
     return white_part;
 }
 
-#pragma endregion
+i2chw_error_t write_data_to_register_by_address(const i2chw_dev_t* i2chw_dev, rgbw_chip_registers_t register_address, uint8_t data)
+{
+    //  Проверяет на ненулевой указатель устройства
 
-#pragma region Public functions
+    if (i2chw_dev == NULL)
+    {
+        return I2CHW_ERR_INVALID_PARAMS;
+    }
+    
+    i2chw_error_t result;
 
-HAL_StatusTypeDef rgbw_driver_init(I2C_HandleTypeDef* hi2c_dev){
+    //  Отправляет адрес rgb-микросхемы расширенный до 8 бит  
 
-    rgbw_chip_registers_t tmp_reg_addres;
-    uint8_t tmp_data[2];
+    uint8_t dev_address_with_command_bit = (i2chw_dev->dev_addr << 1);
 
-    // Сохраняем указатель на I2C
-
-    rgbw_driver_hi2c_dev = hi2c_dev;
-
-    // Включаем микросхему
-
-    tmp_reg_addres = RGBW_CHIP_REG_ENABLED;
-    tmp_data[0] = (uint8_t) tmp_reg_addres;
-    tmp_data[1] = 0x01;
-
-    HAL_StatusTypeDef transmitting_result = HAL_I2C_Master_Transmit(hi2c_dev, (rgb_chip_i2c_adress << 1), tmp_data, 2, RGBW_DRIVER_I2C_TIMEOUT_MS);
-    if(transmitting_result != HAL_OK){
-        return transmitting_result;
+    result = I2CHW_WriteSync(i2chw_dev, &dev_address_with_command_bit, 1);
+    if (result != I2CHW_SUCCESS)
+    {
+        return result;
     }
 
-    // Гасим все каналы
+    //  Отправляет адрес регистра
 
-    tmp_reg_addres = RGBW_CHIP_REG_CHANNELS_MASK;
-    tmp_data[0] = (uint8_t) tmp_reg_addres;
-    tmp_data[1] = 0x00;
-
-    transmitting_result = HAL_I2C_Master_Transmit(hi2c_dev, (rgb_chip_i2c_adress << 1), tmp_data, 2, RGBW_DRIVER_I2C_TIMEOUT_MS);
-    if(transmitting_result != HAL_OK){
-        return transmitting_result;
+    result = I2CHW_WriteSync(i2chw_dev, (uint8_t*) &register_address, 1);
+    if (result != I2CHW_SUCCESS)
+    {
+        return result;
     }
 
-    return transmitting_result;
+    //  Отправляет данные которые должны быть записаны в указанный регистр
+
+    result = I2CHW_WriteSync(i2chw_dev, (uint8_t*) &data, 1);
+    if (result != I2CHW_SUCCESS)
+    {
+        return result;
+    }
+
+    return result;
 }
 
-HAL_StatusTypeDef rgbw_driver_set_channel_brightness(rgbw_driver_channels_t channel, uint8_t brightness){
-    // Проверка на то, что была ли инициализирована микросхема управления RGBW
-    if(rgbw_driver_hi2c_dev == NULL){
-        return HAL_ERROR;
+i2chw_error_t read_data_from_register_by_address(const i2chw_dev_t* i2chw_dev, rgbw_chip_registers_t register_address, uint8_t* data)
+{
+    //  Проверяет на ненулевой указатель устройства
+
+    if (i2chw_dev == NULL)
+    {
+        return I2CHW_ERR_INVALID_PARAMS;
+    }
+    
+    i2chw_error_t result;
+    
+    //  Отправляет адрес rgb-микросхемы расширенный до 8 бит  
+
+    uint8_t dev_address_with_command_bit = (i2chw_dev->dev_addr << 1) | 0b1;
+
+    result = I2CHW_WriteSync(i2chw_dev, &dev_address_with_command_bit, 1);
+    if (result != I2CHW_SUCCESS)
+    {
+        return result;
     }
 
-    HAL_StatusTypeDef result =  HAL_ERROR;
-    rgbw_chip_registers_t tmp_reg_addres;
-    uint8_t tmp_data[2];
+    //  Отправляет адрес регистра и получает его данные, которые должна отправить rgb-микросхема
 
-    // Определяем адрес регистра, в который будем записывать яркость канала
-    tmp_reg_addres = get_addres_from_channel(channel);
-    if(tmp_reg_addres == RGBW_CHIP_REG_INCORRECT){
-        return HAL_ERROR;
-    }
-
-    // Формируем данные(2 байта) для передачи по I2C
-
-    tmp_data[0] = (uint8_t) tmp_reg_addres;
-    tmp_data[1] = brightness;
-
-    // Отправляем данные по I2C
-
-    result = HAL_I2C_Master_Transmit(rgbw_driver_hi2c_dev, (rgb_chip_i2c_adress << 1), tmp_data, 2, RGBW_DRIVER_I2C_TIMEOUT_MS);
-    if(result != HAL_OK){
+    result = I2CHW_WriteReadSync(i2chw_dev, (uint8_t*) &register_address, 1, &data, 1);
+    if (result != I2CHW_SUCCESS)
+    {
         return result;
     }
     
     return result;
 }
 
-HAL_StatusTypeDef rgbw_driver_set_channel_activity(rgbw_driver_channels_t channel, bool activity){
-    // Проверка на то, что была ли инициализирована микросхема управления RGBW
-    if(rgbw_driver_hi2c_dev == NULL){
-        return HAL_ERROR;
+#pragma endregion
+
+#pragma region Public functions
     }
 
     HAL_StatusTypeDef result =  HAL_ERROR;
